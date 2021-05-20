1d482447934a6e247abb2022b27bdd23d77fad67
Jeremy <jeremyeparker@yahoo.com>
2021-05-20 17:50:38 -0400

    first commit

---
+++ src/test/java/com/atsignJar/StudentDatabase/StudentDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsignJar.StudentDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
