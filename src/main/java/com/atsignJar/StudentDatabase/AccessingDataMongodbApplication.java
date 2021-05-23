28fcb3b00aae7fa90a525329c39972e22a38bacb
Jeremy <jeremyeparker@yahoo.com>
2021-05-23 01:02:55 -0400

    class for the application

--- src/main/java/com/atsignJar/StudentDatabase/AccessingDataMongodbApplication.java
+++ src/main/java/com/atsignJar/StudentDatabase/AccessingDataMongodbApplication.java
@@ -1,2 +1,11 @@
-package com.atsignJar.StudentDatabase;public class AccessingDataMongodbApplication {
+package com.atsignJar.StudentDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMongodbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataMongodbApplication.class, args);
+    }
 }
